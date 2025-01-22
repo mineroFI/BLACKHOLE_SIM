a8a752e5137111d65750c383fa505e1c32d5bf01
mineroFI <florencianavarro203@gmail.com>
2025-01-22 18:30:44 -0300

    agrego config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Dimensiones de la Ventana
+WIDTH, HEIGHT = 800, 600
+
+# Colores
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+
+# Masa del agujero negro (para cálculo de gravedad)
+BLACK_HOLE_MASS = 1e6
+
+# Constante Gravitacional
+G = 1e-3
